15f674e7895107224a17f7a42e1cf2e9d6e28010
gutodiasux <gutodiasux@gmail.com>
2023-07-31 19:37:04 -0300

    refact: add optional permissions also in CreateUserRepository

--- src/data/contracts/UserRepository.ts
+++ src/data/contracts/UserRepository.ts
@@ -7,6 +7,7 @@ export namespace CreateUserRepository {
         email: string
         hashedPassword: string
         agreeWithPolicies: boolean
+        permissions?: string[]
     }
     export type Ouput = {
         id: string
