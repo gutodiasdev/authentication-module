9adbacd4d0d94a5437954f07d42c0ba11dd8d8be
gutodiasux <gutodiasux@gmail.com>
2023-07-31 18:54:12 -0300

    feat: add CreateUser interface

---
+++ src/domain/features/CreateUser.ts
@@ -0,0 +1,16 @@
+export interface CreateUSer {
+    execute(input: CreateUser.Input): Promise<CreateUser.Ouput>
+}
+
+export namespace CreateUser {
+    export type Input = {
+        email: string
+        password: string
+        agreeWithPolicies: boolean
+    }
+    export type Ouput = {
+        token: string
+        refreshToken: string
+        permissions: string[]
+    }
+}
