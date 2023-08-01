554d0d8a35304ea7cedaf9b1bc5cf3709c396571
gutodiasux <gutodiasux@gmail.com>
2023-08-01 09:50:29 -0300

    chore(lint): add quotes rules

--- jest.config.js
+++ jest.config.js
@@ -32,7 +32,7 @@ const config = {
   // ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
+  coverageProvider: 'v8',
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
@@ -175,7 +175,7 @@ const config = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    "^.+\\.(t|j).s?$": ["@swc/jest"]
+    '^.+\\.(t|j).s?$': ['@swc/jest']
   }
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
