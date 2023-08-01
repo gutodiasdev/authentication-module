3089bbf8355fe4c29d72ab2357228b086cbc1b47
gutodiasux <gutodiasux@gmail.com>
2023-08-01 19:05:24 -0300

    test: ensure SignInUserUseCase throws EmailOrPasswordAreWrong when findByEmail method return undefined

--- src/__tests__/application/usecases/SignInUserUseCase.spec.ts
+++ src/__tests__/application/usecases/SignInUserUseCase.spec.ts
@@ -1,10 +1,16 @@
+import { MockProxy, mock } from 'jest-mock-extended';
+import { FindUserByEmailRepository } from '../../../data/contracts';
+import { EmailOrPasswordAreWrong } from '../../../domain/error';
 import { SignInUser } from '../../../domain/features';
 
 export class SignInUserUseCase implements SignInUser {
+    constructor(private readonly userRepository: FindUserByEmailRepository) {}
+
     async execute(input: SignInUser.Input): Promise<SignInUser.Output> {
-        input
+        const user = await this.userRepository.findByEmail({ email: input.email })
+        if(!user) throw new EmailOrPasswordAreWrong()
         return {
-            id: '',
+            id: user.id,
             token: '',
             refreshToken: '',
             permissions: ['']
@@ -13,16 +19,25 @@ export class SignInUserUseCase implements SignInUser {
 }
 
 describe('SignInUserUseCase', function () {
+    let userRepository: MockProxy<FindUserByEmailRepository>
     let sut: SignInUserUseCase
 
     beforeEach(() => {
-        sut = new SignInUserUseCase()
+        userRepository = mock()
+        sut = new SignInUserUseCase(userRepository)
+        userRepository.findByEmail.mockResolvedValue({ id: '', password: '', permissions: [''] })
     })
+
     test('it should return user id, token, refresh token and permissions', async function () {
         const result = await sut.execute({ email: '', password: '' })
         expect(result.id).toBeDefined()
         expect(result.token).toBeDefined()
         expect(result.refreshToken).toBeDefined()
         expect(result.permissions).toBeDefined()
+    })
+
+    test('it should throw EmailOrPasswordAreWrong when findByEmail method return undefined', async function () {
+        userRepository.findByEmail.mockResolvedValue(undefined)
+        await expect(() => sut.execute({ email: '', password: '' })).rejects.toThrow(EmailOrPasswordAreWrong)
     })
 })
