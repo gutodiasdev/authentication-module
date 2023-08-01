8f00233ecdf24ef3124bf102bf53ef4f48896c42
gutodiasux <gutodiasux@gmail.com>
2023-08-01 19:58:00 -0300

    test(usecase): ensure SignInUserUseCase throws TokenExpiredError when verify method from TokenIssuer returns false

--- src/__tests__/application/usecases/SignInUserUseCase.spec.ts
+++ src/__tests__/application/usecases/SignInUserUseCase.spec.ts
@@ -1,43 +1,38 @@
 import { MockProxy, mock } from 'jest-mock-extended';
+import { SignInUserUseCase } from '../../../application/usecases';
 import { FindUserByEmailRepository } from '../../../data/contracts';
-import { EmailOrPasswordAreWrong } from '../../../domain/error';
-import { SignInUser } from '../../../domain/features';
-
-export class SignInUserUseCase implements SignInUser {
-    constructor(private readonly userRepository: FindUserByEmailRepository) {}
-
-    async execute(input: SignInUser.Input): Promise<SignInUser.Output> {
-        const user = await this.userRepository.findByEmail({ email: input.email })
-        if(!user) throw new EmailOrPasswordAreWrong()
-        return {
-            id: user.id,
-            token: '',
-            refreshToken: '',
-            permissions: ['']
-        }
-    }
-}
+import { EmailOrPasswordAreWrong, TokenExpiredError } from '../../../domain/error';
+import { TokenIssuer } from '../../../domain/features';
 
 describe('SignInUserUseCase', function () {
+    let tokenIssuer: MockProxy<TokenIssuer>
     let userRepository: MockProxy<FindUserByEmailRepository>
     let sut: SignInUserUseCase
+    const input = { email: '', password: '', token: ''}
 
     beforeEach(() => {
         userRepository = mock()
-        sut = new SignInUserUseCase(userRepository)
+        tokenIssuer = mock()
+        sut = new SignInUserUseCase(userRepository, tokenIssuer)
         userRepository.findByEmail.mockResolvedValue({ id: '', password: '', permissions: [''] })
+        tokenIssuer.verify.mockResolvedValue(true)
+        tokenIssuer.generateToken.mockResolvedValue({ token: '' })
     })
 
     test('it should return user id, token, refresh token and permissions', async function () {
-        const result = await sut.execute({ email: '', password: '' })
+        const result = await sut.execute(input)
         expect(result.id).toBeDefined()
         expect(result.token).toBeDefined()
-        expect(result.refreshToken).toBeDefined()
         expect(result.permissions).toBeDefined()
     })
 
     test('it should throw EmailOrPasswordAreWrong when findByEmail method return undefined', async function () {
         userRepository.findByEmail.mockResolvedValue(undefined)
-        await expect(() => sut.execute({ email: '', password: '' })).rejects.toThrow(EmailOrPasswordAreWrong)
+        await expect(() => sut.execute(input)).rejects.toThrow(EmailOrPasswordAreWrong)
+    })
+
+    test('it should throw TokenExpiredError when verify method from TokenIssuer returns false', async function () {
+        tokenIssuer.verify.mockResolvedValueOnce(false)
+        await expect(() => sut.execute(input)).rejects.toThrow(TokenExpiredError)
     })
 })
