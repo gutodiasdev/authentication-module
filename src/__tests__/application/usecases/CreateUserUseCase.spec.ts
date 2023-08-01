0544b094815cdefd552e19908f0e1f57637e7841
gutodiasux <gutodiasux@gmail.com>
2023-08-01 09:54:24 -0300

    refact(test): remove test since the code is tested in model

--- src/__tests__/application/usecases/CreateUserUseCase.spec.ts
+++ src/__tests__/application/usecases/CreateUserUseCase.spec.ts
@@ -1,10 +1,11 @@
 import { MockProxy, mock } from 'jest-mock-extended'
 import { CreateUserUseCase } from '../../../application/usecases'
-import { CheckUserByEmailRepository, CreateUserRepository } from '../../../data/contracts'
+import { CheckUserByEmailRepository, CreateRefreshTokenRepository, SaveUserRepository } from '../../../data/contracts'
 import { CreateUserFailed, UserAlreadyExistsError } from '../../../domain/error'
 
 describe('CreateUserUseCase', function () {
-    let userRepository: MockProxy<CreateUserRepository & CheckUserByEmailRepository>
+    let userRepository: MockProxy<SaveUserRepository & CheckUserByEmailRepository>
+    let refreshTokenRepository: MockProxy<CreateRefreshTokenRepository>
     let sut: CreateUserUseCase
     const input = {
         email: 'any_email',
@@ -15,7 +16,8 @@ describe('CreateUserUseCase', function () {
 
     beforeEach(() => {
         userRepository = mock()
-        sut = new CreateUserUseCase(userRepository)
+        refreshTokenRepository = mock()
+        sut = new CreateUserUseCase(userRepository, refreshTokenRepository)
         userRepository.checkByEmail.mockResolvedValue(undefined)
     })
 
@@ -25,25 +27,14 @@ describe('CreateUserUseCase', function () {
         expect(result.refreshToken).toBeDefined()
         expect(result.permissions).toBeDefined()
     })
-
-    test('it should call createUser method with correct params', async function () {
-        userRepository.createUser.mockResolvedValue({ id: '' })
-        await sut.execute(input)
-        expect(userRepository.createUser).toHaveBeenCalledWith({
-            email: 'any_email',
-            hashedPassword: 'any_password',
-            agreeWithPolicies: true,
-            permissions: ['any.permission']
-        })
-    })
-
+    
     test('it should throw UserAlreadyExistsError if checkByEmail method return value', async function () {
         userRepository.checkByEmail.mockResolvedValueOnce({ id: '' })
         await expect(() => sut.execute(input)).rejects.toThrow(UserAlreadyExistsError)
     })
 
     test('it should throw CreateUserFailed if createUser method fails', async function () {
-        userRepository.createUser.mockRejectedValueOnce({})
+        userRepository.saveUser.mockRejectedValueOnce({})
         await expect(() => sut.execute(input)).rejects.toThrow(CreateUserFailed)
     })
 })
