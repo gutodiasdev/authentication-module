f4ed4129e9d98a1ededcedf4092e140f041d3b50
gutodiasux <gutodiasux@gmail.com>
2023-08-01 19:52:48 -0300

    refact(feature): add token as a input in SignInUser interface

--- src/domain/features/SignInUser.ts
+++ src/domain/features/SignInUser.ts
@@ -6,11 +6,11 @@ export namespace SignInUser {
     export type Input = {
         email: string
         password: string
+        token: string
     }
     export type Output = {
         id: string
         token: string
-        refreshToken: string
         permissions: string[]
     }
 }
