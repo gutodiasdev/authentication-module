920cc9590208292e7e159e01ab8a3cb32b5d7bcd
gutodiasux <gutodiasux@gmail.com>
2023-08-01 09:52:48 -0300

    refact(usecase): implement token creation in CreateUserUseCase

--- src/application/usecases/CreateUserUseCase.ts
+++ src/application/usecases/CreateUserUseCase.ts
@@ -1,26 +1,46 @@
-import { CheckUserByEmailRepository, CreateUserRepository } from "../../data/contracts"
-import { CreateUserFailed, UserAlreadyExistsError } from "../../domain/error"
-import { CreateUSer, CreateUser } from "../../domain/features"
+import { randomUUID } from 'crypto'
+import { sign } from 'jsonwebtoken'
+import {
+    CheckUserByEmailRepository,
+    CreateRefreshTokenRepository,
+    SaveUserRepository
+} from '../../data/contracts'
+import { CreateUserFailed, UserAlreadyExistsError } from '../../domain/error'
+import { CreateUSer, CreateUser } from '../../domain/features'
+import { User } from '../../domain/models'
 
 export class CreateUserUseCase implements CreateUSer {
-    constructor(private readonly userRepository: CreateUserRepository & CheckUserByEmailRepository) { }
+    constructor(
+        private readonly userRepository: SaveUserRepository & CheckUserByEmailRepository,
+        private readonly refreshTokenRepository: CreateRefreshTokenRepository
+    ) { }
 
     async execute(input: CreateUser.Input): Promise<CreateUser.Ouput> {
         const userAlreadyExists = await this.userRepository.checkByEmail({ email: input.email })
         if (userAlreadyExists) throw new UserAlreadyExistsError()
         try {
-            await this.userRepository.createUser({
+            const user = User.create({
                 email: input.email,
-                hashedPassword: input.password,
-                agreeWithPolicies: input.agreeWithPolicies,
+                password: input.password,
+                agreeWithPolicies: input.agreeWithPolicies
+            })
+            await this.userRepository.saveUser({
+                id: user.id,
+                email: user.email,
+                hashedPassword: user.password,
+                agreeWithPolicies: user.agreeWithPolicies,
                 permissions: input.permissions
             })
+            const token = sign({ userId: user.id, permissions: user.permissions }, 'TOKEN_SECRET', { expiresIn: '15m' })
+            const refreshToken = randomUUID()
+            await this.refreshTokenRepository.createRefreshToken({ userId: user.id, token: refreshToken })
+            
             return {
-                token: '',
-                refreshToken: '',
-                permissions: ['']
+                token,
+                refreshToken,
+                permissions: user.permissions
             }
-        } catch(error: any){
+        } catch (error: any) {
             throw new CreateUserFailed(error.message)
         }
     }
