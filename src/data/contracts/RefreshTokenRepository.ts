d7507b59ca13a2cb0e1acec021a9a23500ea2a26
gutodiasux <gutodiasux@gmail.com>
2023-08-02 14:53:50 -0300

    feat(repository): add FindRefreshTokenRepository interface

--- src/data/contracts/RefreshTokenRepository.ts
+++ src/data/contracts/RefreshTokenRepository.ts
@@ -11,3 +11,17 @@ export namespace CreateRefreshTokenRepository {
         refreshToken: string
     }
 }
+
+export interface FindRefreshTokenRepository {
+    findToken(input: FindRefreshTokenRepository.Input): Promise<FindRefreshTokenRepository.Output>
+}
+
+export namespace FindRefreshTokenRepository {
+    export type Input = {
+        token: string
+    }
+    export type Output = {
+        token: string
+        expirationDate: string
+    }
+}
