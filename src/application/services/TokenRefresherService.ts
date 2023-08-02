7be84d52fa72cf456fdb4c2958ce80af5aafca98
gutodiasux <gutodiasux@gmail.com>
2023-08-02 16:18:23 -0300

    refact(service): add FindRefreshTokenRepository & UpdateRefreshTokenRepository as a constructor dependency in TokenRefresherService

--- src/application/services/TokenRefresherService.ts
+++ src/application/services/TokenRefresherService.ts
@@ -1,23 +1,26 @@
-import { FindRefreshTokenRepository } from '../../data/contracts'
+import { randomUUID } from 'crypto'
+import { FindRefreshTokenRepository, UpdateRefreshTokenRepository } from '../../data/contracts'
 import { InvalidRefreshTokenError, RefreshTokenIsExpiredError } from '../../domain/error'
 import { DateHandler, TokenIssuer, TokenRefresher } from '../../domain/features'
 
 export class TokenRefresherService implements TokenRefresher {
     constructor(
         private readonly tokenIssuerService: TokenIssuer,
-        private readonly refreshTokenRepository: FindRefreshTokenRepository,
+        private readonly refreshTokenRepository: FindRefreshTokenRepository & UpdateRefreshTokenRepository,
         private readonly dateService: DateHandler
-    ) {}
-    
+    ) { }
+
     async execute(input: TokenRefresher.Input): Promise<TokenRefresher.Output> {
         const refreshToken = await this.refreshTokenRepository.findToken({ token: input.refreshToken })
-        if(!refreshToken) throw new InvalidRefreshTokenError()
-        const isAfter = this.dateService.isAfter({ fromDate: refreshToken.expirationDate, toDate: this.dateService.getNow()})
+        if (!refreshToken) throw new InvalidRefreshTokenError()
+        const isAfter = this.dateService.isAfter({ fromDate: refreshToken.expirationDate, toDate: this.dateService.getNow() })
         if (!isAfter) throw new RefreshTokenIsExpiredError()
+        const newRefreshToken = randomUUID()
         const tokenIssuer = this.tokenIssuerService.generateToken({ id: input.id, permissions: input.persmissions })
+        await this.refreshTokenRepository.updateRefreshToken({ token: newRefreshToken, expirationDate: this.dateService.getExpirationDate({ addDaysFromNow: 30 }) })
         return {
             token: tokenIssuer.token,
-            refreshToken: ''
+            refreshToken: newRefreshToken
         }
     }
 }
