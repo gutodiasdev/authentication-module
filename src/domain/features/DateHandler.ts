59ea308766670cac951e00415f7ee96deefed756
gutodiasux <gutodiasux@gmail.com>
2023-08-02 16:19:16 -0300

    feat(interface): add getExpirationDate in DateHandler interface

--- src/domain/features/DateHandler.ts
+++ src/domain/features/DateHandler.ts
@@ -1,6 +1,7 @@
 export interface DateHandler {
     isAfter(input: DateHandler.isAfter.Input): DateHandler.isAfter.Ouput
     getNow(): DateHandler.getNow.Ouput
+    getExpirationDate(input: DateHandler.getExpirationDate.Input): DateHandler.getExpirationDate.Ouput
 }
 
 export namespace DateHandler.isAfter {
@@ -12,5 +13,12 @@ export namespace DateHandler.isAfter {
 }
 
 export namespace DateHandler.getNow {
+    export type Ouput = string
+}
+
+export namespace DateHandler.getExpirationDate {
+    export type Input = {
+        addDaysFromNow: number
+    }
     export type Ouput = string
 }
